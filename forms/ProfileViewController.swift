cdd2bf66fda7d76e42819f9ae19d51589ff19354
buriedguns <blivion21@yandex.ru>
2018-11-30 19:45:58 +0300

    first

---
+++ forms/ProfileViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileViewController.swift
+//  forms
+//
+//  Created by Макс on 30/11/2018.
+//  Copyright © 2018 Макс. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+
+    @IBOutlet weak var userName: UILabel!
+    @IBAction func logOutButton(_ sender: UIButton) {
+        //removing values from default
+        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        UserDefaults.standard.synchronize()
+        
+        //switching to login screen
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        self.navigationController?.pushViewController(loginViewController, animated: true)
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //hiding back button
+        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: navigationController, action: nil)
+        navigationItem.leftBarButtonItem = backButton
+        
+        //getting user data from defaults
+        let defaultValues = UserDefaults.standard
+        if let name = defaultValues.string(forKey: "username"){
+            //setting the name to label
+            userName.text = name
+        }else{
+            //send back to login view controller
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+}
